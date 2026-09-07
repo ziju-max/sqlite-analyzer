99b7181e5ed59d1e6c11bb8c5ed86f678b4da12d
ziju-max <204708525@qq.com>
2026-09-07 20:32:34 +0800

    初始化

---
+++ src/main/java/org/csu/sqliteanalyzer/SqliteAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.csu.sqliteanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqliteAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqliteAnalyzerApplication.class, args);
+    }
+
+}
